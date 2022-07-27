5aa5f640cfc577f3b223edb114c1be2d7e95b58f
Gustavo Teixeira Albino <gustavo-t@ufmg.br>
2022-07-27 15:56:52 -0300

    Show responses
    show responses in HTML page

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 function consultarCep() {
-    let cep = document.querySelector('#cep').value
-    let localizacao = document.querySelector('#localizacao')
+    let cep = document.getElementById("cep").value
     let url = `https://viacep.com.br/ws/${cep}/json/`
+
+    
     $.ajax({
         url: url,
         type: 'GET',
         success: function(response) {
-            localizacao.innerHTML = `${response}`
+
+            $('.uf').html(response.uf);
+            $('.localidade').html(response.localidade)
+            $('.bairro').html(response.bairro)
+            $('.logradouro').html(response.logradouro)
+            $('.ddd').html(response.ddd)
         }
     })
 }
